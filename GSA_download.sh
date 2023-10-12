ad42b22d99a4cf2a91e7a7eb8a01fdaacedef5bb
linxy29 <xinyi_lin@outlook.com>
2023-10-12 09:47:26 +0800

    modified GSA_download

--- GSA_download.sh
+++ GSA_download.sh
@@ -1,6 +1,9 @@
+## only one command is enough
+wget -c --user="jwkho@hku.hk" --password="HoLab2013!"  --mirror ftp://human.big.ac.cn/HRA000183
+
 #!/bin/bash
-target=HRA000183
-for i in `cat HRA000183.txt|tr  "\t" "/"`;do 
-echo ${i};
-wget -c --user="jwkho@hku.hk" --password="HoLab2013!"  --mirror ftp://human.big.ac.cn/${target}/${i}
-done
+#target=HRA000183
+#for i in `cat HRA000183.txt|tr  "\t" "/"`;do 
+#echo ${i};
+#wget -c --user="jwkho@hku.hk" --password="HoLab2013!"  --mirror ftp://human.big.ac.cn/${target}/${i}
+#done
